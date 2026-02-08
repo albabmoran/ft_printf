b9aebe19f8c70a074e9c216742910b627c59fd41
Alba Ben Amed Moran <albben-a@student.42madrid.com>
2026-02-08 16:25:53 +0100

    main modified

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: albben-a <albben-a@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 13:47:02 by albben-a          #+#    #+#             */
-/*   Updated: 2026/02/07 19:27:03 by albben-a         ###   ########.fr       */
+/*   Updated: 2026/02/08 16:23:25 by albben-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,72 +60,4 @@ int	ft_printf(char const *formatstr, ...)
 	va_end(args);
 	return (countb);
 }
-/*
-#include <stdio.h>
 
-int	main(void)
-{
-	ft_printf("%d%d%d\n", 1, 2, 3);
-	printf("%d%d%d\n", 1, 2, 3);
-
-	ft_printf("%s%d%c%u%x%X%%\n", "hi", -42, 'A', 42, 255, 255);
-	printf("%s%d%c%u%x%X%%\n", "hi", -42, 'A', 42, 255, 255);
-
-	ft_printf("A:%d B:%d C:%d\n", 10, 20, 30);
-	printf("A:%d B:%d C:%d\n", 10, 20, 30);
-
-	ft_printf("%%%%%d%%%%\n", 42);
-	printf("%%%%%d%%%%\n", 42);
-	
-	ft_printf("ft_printf: %s %d %c %u %%\n", "Edad:", 20, 'A', 42);
-	printf("printf: %s %d %c %u %%\n", "Edad:", 20, 'A', 42);
-
-	return (0);
-}*/
-/*
-#include <stdio.h>
-#include <limits.h>
-
-int main(void)
-{
-    int ret_ft;
-    int ret_real;
-    //int x = 42;
-
-    printf("=== TEST 1 ===\n");
-    ret_ft = ft_printf("Hola %s %d %c %%\n", "mundo", 42, 'A');
-    ret_real = printf("Hola %s %d %c %%\n", "mundo", 42, 'A');
-    printf("ft: %d | real: %d\n\n", ret_ft, ret_real);
-
-    printf("=== TEST 2 ===\n");
-    ret_ft = ft_printf("%d %d %d\n", 1, 2, 3);
-    ret_real = printf("%d %d %d\n", 1, 2, 3);
-    printf("ft: %d | real: %d\n\n", ret_ft, ret_real);
-
-    printf("=== TEST 3 ===\n");
-    ret_ft = ft_printf("[%c][%s][%d]\n", 'A', "BCD", 42);
-    ret_real = printf("[%c][%s][%d]\n", 'A', "BCD", 42);
-    printf("ft: %d | real: %d\n\n", ret_ft, ret_real);
-
-    printf("=== TEST 4 === (NULL y edge)\n");
-    ret_ft = ft_printf("NULL: %s | INT_MIN: %d\n", (char *)NULL, INT_MIN);
-    ret_real = printf("NULL: %s | INT_MIN: %d\n", (char *)NULL, INT_MIN);
-    printf("ft: %d | real: %d\n\n", ret_ft, ret_real);
-
-    printf("=== TEST 5 === (punteros y hex)\n");
-    ret_ft = ft_printf("%p %x %X\n", NULL, 255, 255);
-    ret_real = printf("%p %x %X\n", NULL, 255, 255);
-    printf("ft: %d | real: %d\n\n", ret_ft, ret_real);
-
-    printf("=== TEST 6 === (char nulo)\n");
-    ret_ft = ft_printf("A%cB%dC\n", '\0', 42);
-    ret_real = printf("A%cB%dC\n", '\0', 42);
-    printf("\nft: %d | real: %d\n\n", ret_ft, ret_real);
-
-    printf("=== TEST 7 === (porcentajes)\n");
-    ret_ft = ft_printf("%%%%%d%%%%\n", 42);
-    ret_real = printf("%%%%%d%%%%\n", 42);
-    printf("ft: %d | real: %d\n\n", ret_ft, ret_real);
-
-    return 0;
-}*/
